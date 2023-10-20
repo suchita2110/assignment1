a8edf8c43bbe2a19b02762c0f4407b6ea8ac48fc
suchitagurme <suchitagurme@gmail.com>
2023-10-20 19:50:29 +0530

    Events added on the objects

--- src/app/box-comp/box-comp.component.ts
+++ src/app/box-comp/box-comp.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./box-comp.component.css']
 })
 export class BoxCompComponent {
-
+ handleBoxEvent(){
+  alert("Box Clicked!")
+ }
 }
--- src/app/circle-comp/circle-comp.component.ts
+++ src/app/circle-comp/circle-comp.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./circle-comp.component.css']
 })
 export class CircleCompComponent {
-
+handleCircleEvent(){
+  alert("Circle Cliked!")
+}
 }
